4b4d03204e0c72cd6dff45f73b5317fa35042909
Joonas Bergius <joonas@cosmonic.com>
2024-10-29 14:31:03 -0500

    chore: Modernize Golang component
    Signed-off-by: Joonas Bergius <joonas@cosmonic.com>

--- components/golang/hello.go
+++ components/golang/hello.go
@@ -1,66 +1,61 @@
+//go:generate go run github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go generate --world hello --out gen ./wit
+
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"strings"
 
-	gen "github.com/wasmcloud/wasmcloud/examples/golang/components/http-hello-world/gen"
+	"github.com/bytecodealliance/wasm-tools-go/cm"
+	"github.com/wasmcloud/wasmcloud/examples/golang/components/http-hello-world/gen/wasmcloud/couchbase/v0.1.0-draft/document"
+	"github.com/wasmcloud/wasmcloud/examples/golang/components/http-hello-world/gen/wasmcloud/couchbase/v0.1.0-draft/types"
+	"go.wasmcloud.dev/component/net/wasihttp"
 )
 
-// Helper type aliases to make code more readable
-type HttpRequest = gen.ExportsWasiHttp0_2_0_IncomingHandlerIncomingRequest
-type HttpResponseWriter = gen.ExportsWasiHttp0_2_0_IncomingHandlerResponseOutparam
-type HttpOutgoingResponse = gen.WasiHttp0_2_0_TypesOutgoingResponse
-type HttpError = gen.WasiHttp0_2_0_TypesErrorCode
-
-type Document = gen.WasmcloudCouchbase0_1_0_draft_DocumentDocument
-
-type HttpServer struct{}
-
 func init() {
-	httpserver := HttpServer{}
-	// Set the incoming handler struct to HttpServer
-	gen.SetExportsWasiHttp0_2_0_IncomingHandler(httpserver)
+	wasihttp.HandleFunc(documentHandler)
 }
 
-func (h HttpServer) Handle(request HttpRequest, responseWriter HttpResponseWriter) {
-	// Construct HttpResponse to send back
-	headers := gen.NewFields()
-	httpResponse := gen.NewOutgoingResponse(headers)
-	httpResponse.SetStatusCode(200)
-	body := httpResponse.Body().Unwrap()
-	bodyWrite := body.Write().Unwrap()
-
+func documentHandler(w http.ResponseWriter, r *http.Request) {
 	// Get document body & id
-	documentContents := request.Consume().Unwrap().Stream().Unwrap().BlockingRead(99999).Unwrap()
-	var documentId string
-	if request.PathWithQuery().IsSome() {
-		trimmed := strings.TrimLeft(request.PathWithQuery().Unwrap(), "/")
-		if trimmed == "" {
-			documentId = "demodoc"
-		} else {
-			documentId = trimmed
-		}
-	} else {
-		documentId = "demodoc"
+	documentId := "demodoc"
+	trimmed := strings.TrimLeft(r.URL.Path, "/")
+	if trimmed != "" {
+		documentId = trimmed
+	}
+
+	var input map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Provided input could not be decoded, please check that your input is valid JSON.", http.StatusBadRequest)
+		return
+	}
+
+	output, err := json.Marshal(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	// Upsert and get the document
-	document := gen.WasmcloudCouchbase0_1_0_draft_TypesDocumentRaw(string(documentContents))
-	gen.WasmcloudCouchbase0_1_0_draft_DocumentUpsert(documentId, document, gen.None[gen.WasmcloudCouchbase0_1_0_draft_DocumentDocumentUpsertOptions]())
-	res := gen.WasmcloudCouchbase0_1_0_draft_DocumentGet(documentId, gen.None[gen.WasmcloudCouchbase0_1_0_draft_DocumentDocumentGetOptions]())
+	doc := types.DocumentRaw(types.JSONString(string(output)))
+	upsert := document.Upsert(types.DocumentID(documentId), doc, cm.None[document.DocumentUpsertOptions]())
+	if upsert.IsErr() {
+		http.Error(w, upsert.Err().String(), http.StatusInternalServerError)
+		return
+	}
+
+	res := document.Get(types.DocumentID(documentId), cm.None[document.DocumentGetOptions]())
+	if res.IsErr() {
+		http.Error(w, res.Err().String(), http.StatusInternalServerError)
+		return
+	}
 
 	// Send HTTP response
-	okResponse := gen.Ok[HttpOutgoingResponse, HttpError](httpResponse)
-	gen.StaticResponseOutparamSet(responseWriter, okResponse)
-	if res.IsOk() {
-		document := res.Unwrap()
-		bodyWrite.BlockingWriteAndFlush([]uint8(document.Document.GetRaw())).Unwrap()
-	} else {
-		bodyWrite.BlockingWriteAndFlush([]uint8("Document not found")).Unwrap()
-	}
-	bodyWrite.Drop()
-	gen.StaticOutgoingBodyFinish(body, gen.None[gen.WasiHttp0_2_0_TypesTrailers]())
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(*res.OK().Document.Raw())
 }
 
-//go:generate wit-bindgen tiny-go wit --out-dir=gen --gofmt
 func main() {}
